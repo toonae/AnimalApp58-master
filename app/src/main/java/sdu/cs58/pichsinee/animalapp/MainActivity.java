bf7783df7175666fac80f5015712119f40acb8c0
supanida <sup.ta1909@gmail.com>
2018-03-14 11:58:39 +0700

    Design Main UI

---
+++ app/src/main/java/sdu/cs58/pichsinee/animalapp/MainActivity.java
@@ -0,0 +1,15 @@
+package sdu.cs58.pichsinee.animalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }//end
+
+}//end Class
